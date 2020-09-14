60edd13ce3cfb212312fdeecf67ae65321c00894
localghost4000 <19227912+localghost4000@users.noreply.github.com>
2020-09-14 11:50:30 +1000

    Add line items

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -19,12 +19,13 @@ class LineItemsController < ApplicationController
 
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
+    @line_item = @cart.add_product(product)
 
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
         format.json { render :show, status: :created, location: @line_item }
+        session[:counter] = 0
       else
         format.html { render :new }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
